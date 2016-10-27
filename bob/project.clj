3cdc78d5d455119d5fce26d6db2f5d80ca80d826
Florian Fischer <findingharrylime@gmail.com>
2016-10-27 18:42:59 +0200

    Add functios for checking yelling and asking
    yelling predicate based on Clojure Cookbook recipe
    
    3 Failing tests

---
+++ bob/project.clj
@@ -0,0 +1,5 @@
+(defproject bob "0.1.0-SNAPSHOT"
+  :description "bob exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/bob"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]])
---
+++ bob/src/bob.clj
@@ -0,0 +1,20 @@
+(ns bob)
+
+(defn yelling? [s]
+    (every? #(or (not (Character/isLetter %))
+                 (Character/isUpperCase %))
+            s))
+
+(defn asking? [s]
+  (= "?" (last s)))
+
+(defn nothing? [s]
+  (clojure.string/blank? s))
+
+(defn response-for [s] ;; <- arglist goes here
+  ;; your code goes here
+  (cond
+    (nothing? s) "Fine. Be that way!"
+    (yelling? s) "Whoa, chill out!"
+    (asking? s) "Sure"
+     :else "Whatever."))
