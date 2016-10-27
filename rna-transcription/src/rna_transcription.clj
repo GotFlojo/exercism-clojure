8c2b86e438fb3444ba3487829ab1b646e9a83c50
Florian Fischer <findingharrylime@gmail.com>
2016-10-27 19:51:56 +0200

    Add Exception via if-let
    Check to see if something is in the map and throw an AssertionError if
    not

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -2,4 +2,7 @@
 
 (defn to-rna [s] ;; <- arglist goes here
   ;; your code goes here
-  (apply str (map #({\G \C, \C \G, \T \A, \A \U} %) s)))
+  (apply str (map #(if-let [rna ({\G \C, \C \G, \T \A, \A \U} %)]
+                     rna
+                     (throw  (AssertionError. (str "No valid DNA base: " %))))
+                  s)))
