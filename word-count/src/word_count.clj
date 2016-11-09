7c5913e831dc0577a590f59b0d6e41c17ed39158
Florian Fischer <findingharrylime@gmail.com>
2016-11-09 22:05:09 +0100

    Modify own frequencies implementation
    Add default argument to get for arrays
    directly assoc with default of 0 and increment if no element is in map
    yet

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -2,7 +2,21 @@
 
 (defn words [s]
   (clojure.string/split
-    (clojure.string/lower-case s) #"[^\w]+"))
+    (clojure.string/lower-case s) #"[\W]+"))
+
+(defn filter-same [coll item]
+  (filter #(= item %) coll))
+
+;; reduce collection to map
+;; start with empty map
+;; take elemnt
+;; see if key with elementname exists
+;; if yes get value and increment it
+;; if no add key with value 1
+(defn frequency [c]
+  (reduce (fn [dict elem]
+              (assoc dict elem (inc (dict elem 0)))) {} c))
 
 (defn word-count [s]
-  (into {} (frequencies (words s)))) ;; <- arglist goes here(clojure.string/split s)
+  ;;(into {} (frequencies (words s))))
+  (frequency (words s)))
