7e14b08322be28ca40aaf2e9b107206f4d56f6a6
Florian Fischer <findingharrylime@gmail.com>
2016-11-23 21:54:32 +0100

    Second implementation of anagram. With frquencies

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,11 +1,14 @@
 (ns anagram)
 
+(defn char-frequencies [string]
+  (into {} (frequencies (seq string))))
+
 (defn is-anagram? [word candidate]
   (let [w (clojure.string/lower-case word)
         c (clojure.string/lower-case candidate)]
     (if (= c w)
       false
-      (= (sort (seq w)) (sort (seq c))))))
+      (= (char-frequencies w) (char-frequencies c)))))
 
 (defn anagrams-for [word candidates]
   (filter (partial is-anagram? word) candidates))
